60e9ff587bbf27c3fd7cda123e0e541a57588fb4
dhairyagupta07 <dhairya71106@gmail.com>
2026-08-24 11:13:15 +0530

    registerUser controllers

---
+++ Backend/controllers/user.controllers.js
@@ -0,0 +1,31 @@
+import User from "../models/user.model"
+
+export const registerUser = async(req, res) =>{
+
+    const {name, username, email, password} = req.body
+
+    // validations
+    try{
+        if(!username || !name || !password || !email){
+            return res.status(422).json({message: 'All fields required'})
+        }
+        // does username exists
+        const userNameExists = await User.findOne({username})
+        if(userNameExists){
+            return res.status(400).json({message: 'username already exists'})
+        }
+        const emailExists = await User.findOne({email})
+        if(emailExists){
+            return res.status(400).json({message: 'email already exists'})
+        }
+
+        if(password.length <= 6){
+            return res.status(400).json({message: 'password length should be greater than 6 character'})
+        }
+
+        User.create({username, name, password, email})
+    }
+    catch{
+
+    }
+}
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const userRoutes = express.Router()
+
+// Register User
+
+userRoutes.post('/register')
+
+// Login User
+
+export default userRoutes
