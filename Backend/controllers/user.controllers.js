a10fff0219f7f83ef2482bb497318d2714e1963d
dhairyagupta07 <dhairya71106@gmail.com>
2026-08-25 11:07:45 +0530

    push JWT

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcrypt from 'bcrypt'
+import genToken from "../utils/genToken.js"
 
 export const registerUser = async(req, res) =>{
 
@@ -11,8 +12,8 @@ export const registerUser = async(req, res) =>{
             return res.status(422).json({message: 'All fields required'})
         }
         // does username exists
-        const userNameExists = await User.findOne({username})
-        if(userNameExists){
+        const user = await User.findOne({username})
+        if(user){
             return res.status(400).json({message: 'username already exists'})
         }
         const emailExists = await User.findOne({email})
@@ -23,12 +24,44 @@ export const registerUser = async(req, res) =>{
         if(password.length < 6){
             return res.status(400).json({message: 'password length should be greater than or Equal to 6'})
         }
+        const salt = await bcrypt.genSalt(10);
+        //console.log(salt);
         const hashedPassword = bcrypt.hashSync(password, 10)
 
         const newUser = await User.create({username, name, password: hashedPassword, email})
+
+        // Generate JWT 
+        const token = genToken(newUser._id)
+        //console.log(token)
+
         res.status(201).json(newUser)
     }
     catch{
         res.status(500).json({message: "Internal Server Error"})
+    }
+}
+
+export const loginUser = async(req, res) =>{
+    // Login the user
+    try {
+        const {email, password} = req.body
+
+        if(!email || !password){
+            return res.status(422).json({message: 'All fields required'})
+        }
+
+        const userExists = await User.findOne({email})
+        if(!userExists){
+            return res.status(404).json({message: 'User Not Found'})
+        }
+
+        const correctPassword = bcrypt.compareSync(password, userExists.password);
+        if(!correctPassword){
+            return res.status(401).json({message: 'User Not Found'})
+        }
+
+        res.status(200).json({message: 'Login Successful', user: userExists})
+    } catch (error) {
+        return res.status(500).json({message: 'Internal Server Error'})
     }
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { registerUser } from '../controllers/user.controllers.js'
+import { registerUser, loginUser } from '../controllers/user.controllers.js'
 
 const userRoutes = express.Router()
 
 // Register User
 
 userRoutes.post('/register', registerUser)
+userRoutes.post('/login', loginUser)
 
 // Login User
 
